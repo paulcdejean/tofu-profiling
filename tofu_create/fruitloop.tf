55ec0bd17a8dfc95b59e1ba5c39810d376dafb42
Paul Dejean <paulcdejean@gmail.com>
2026-06-09 18:35:01 -0500

    10k sqs queues.

--- tofu_create/fruitloop.tf
+++ tofu_create/fruitloop.tf
@@ -1,17 +1,35 @@
 locals {
   fruits_folder = abspath("${path.module}/../fruits/")
-  fruit_files = fileset("${local.fruits_folder}/**", "*.yaml")
+  fruit_files   = fileset("${local.fruits_folder}/**", "*.yaml")
   fruit_set = toset([
     for file in local.fruit_files : abspath("${path.module}/../fruits/${trimprefix(file, "../")}")
   ])
-  fruit_map = {
-    for file in local.fruit_set : element(split("/", dirname(file)), -1) => toset(yamldecode(file(file)).colors)
-  }
+}
+
+locals {
+  animals = toset([
+    "lion",
+    "elephant",
+    "dolphin",
+    "eagle",
+    "octopus",
+    "cheetah",
+    "gorilla",
+    "penguin",
+    "wolf",
+    "axolotl",
+  ])
+
+  fruit_map = merge([
+    for animal in local.animals : {
+      for file in local.fruit_set : "${animal}-${element(split("/", dirname(file)), -1)}" => toset(yamldecode(file(file)).colors)
+    }
+  ]...)
 }
 
 module "fruit" {
   for_each = local.fruit_map
-  source = "${path.module}/modules/fruit"
-  fruit  = each.key
-  colors = each.value
+  source   = "${path.module}/modules/fruit"
+  fruit    = each.key
+  colors   = each.value
 }
